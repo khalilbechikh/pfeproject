7b61fc58fc0871b35820ab672c8d3fb4349fdb6b
jobran <jobran628@gmail.com>
2025-04-22 22:03:00 +0100

    node modules added

--- WEB/BACKEND/CODE/src/index.ts
+++ WEB/BACKEND/CODE/src/index.ts
@@ -1,16 +1,20 @@
 import 'reflect-metadata';
 import express, { Request, Response } from 'express';
 import { configureRoutes } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './docs/swagger';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
+// Swagger UI endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Welcome  User mangementdfsaf API! Use /api routes to access the API.');
+  res.send('Welcome User Managsdgfgdement API! Use /api routes to access the API.');
 });
 
 // Configure and mount API routes
@@ -19,6 +23,7 @@ app.use('/v1/api', configureRoutes());
 // Start server
 app.listen(port, async () => {
   console.log(`Server running at http://localhost:${port}`);
+  console.log(`API Documentation available at http://localhost:${port}/api-docs`);
 });
 
 export default app;
